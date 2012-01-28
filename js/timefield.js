24039a0e58d763fb19a5532fda2822cd705915b7
Wesley Nichols <wesley.o.nichols@gmail.com>
2012-01-28 11:35:18 -0500

    fixed widget parameters not being passed reported in application queue

--- js/timefield.js
+++ js/timefield.js
@@ -6,9 +6,14 @@
 (function ($) {
   Drupal.behaviors.timefield = {    
     attach: function(context, settings) {
-      $(".edit-timefield-timepicker", context).each(function(index) {      
-        $(this).timepicker(settings.timefield);
-      });    
+      
+      // Iterate over timefield settings, which keyed by input class.
+      for (var element in settings.timefield) {
+        // Attach timepicker behavior to each matching element.
+        $("input.edit-timefield-timepicker." + element, context).each(function(index) {          
+          $(this).timepicker(settings.timefield[element]);
+        });
+      }
     }
   };
 })(jQuery);
