a810361bc8b987a30f4808a8c138db7968de87af
Wesley Nichols <wesley.o.nichols@gmail.com>
2012-01-28 11:42:20 -0500

    removed last vestige of timepicker code from repo

--- js/timefield.js
+++ js/timefield.js
@@ -4,13 +4,13 @@
  */
 
 (function ($) {
-  Drupal.behaviors.timefield = {    
+  Drupal.behaviors.timefield = {
     attach: function(context, settings) {
-      
+
       // Iterate over timefield settings, which keyed by input class.
       for (var element in settings.timefield) {
         // Attach timepicker behavior to each matching element.
-        $("input.edit-timefield-timepicker." + element, context).each(function(index) {          
+        $("input.edit-timefield-timepicker." + element, context).each(function(index) {
           $(this).timepicker(settings.timefield[element]);
         });
       }
